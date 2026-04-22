51c21bad2dab2e6ff903b55105dd3898a1e9cd90
Mackenzie McIntyre <362574782@GoTVDSB.ca>
2026-04-22 13:23:32 -0400

    done

--- Game1.cs
+++ Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace animation_lesson
 {
@@ -9,6 +10,7 @@ namespace animation_lesson
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        
         Texture2D tribblebrownTexture, tribblegreyTexture, tribblecreamTexture, fur4Texture;
 
         SoundEffect tribbleSound;
@@ -19,11 +21,18 @@ namespace animation_lesson
         Rectangle tribbleBrownRect;
         Rectangle tribbleGreyRect;
         Rectangle tribbleCreamRect;
+        Rectangle fur4Rect;
+
+        Color backColor, brownColorMask;
 
 
         Vector2 tribblebrownSpeed;
         Vector2 tribblegreySpeed;
         Vector2 tribblecreamSpeed;
+        Vector2 fur4Speed;
+
+
+        int randNum;
 
         public Game1()
         {
@@ -36,18 +45,39 @@ namespace animation_lesson
         {
             // TODO: Add your initialization logic here
 
+            Random generator = new Random();
+            brownColorMask = Color.White;
+            
+            randNum = generator.Next(1, 4);
+
+            if(randNum == 1)
+            {
+                fur4Rect = new Rectangle(400, 10, 100, 100);
+            }
+            else if(randNum == 2)
+            {
+                fur4Rect = new Rectangle(400, 10, 200, 50);
+            }
+            else if (randNum == 3)
+            {
+                fur4Rect = new Rectangle(400, 10, 20, 40);
+            }
+
+
             tribbleBrownRect = new Rectangle(301, 10, 100, 100);
             tribbleGreyRect = new Rectangle(400, 12, 100, 100);
             tribbleCreamRect = new Rectangle(400, 12, 100, 100);
+            
 
             window = new Rectangle(0, 0, 800, 600);
             _graphics.PreferredBackBufferWidth =window.Width;
             _graphics.PreferredBackBufferHeight =window.Height;
             _graphics.ApplyChanges();
 
-            tribblebrownSpeed = new Vector2(2,2);
-            tribblegreySpeed = new Vector2(4, 2);
+            tribblebrownSpeed = new Vector2(1,2);
+            tribblegreySpeed = new Vector2(4, 7);
             tribblecreamSpeed = new Vector2(6, 2);
+            fur4Speed = new Vector2(2, 2);
 
 
             base.Initialize();
@@ -82,6 +112,7 @@ namespace animation_lesson
             {
                 tribblebrownSpeed.X *= -1;
                 tribbleSound.Play();
+                backColor = Color.Orange;
                 
 
             }
@@ -91,6 +122,7 @@ namespace animation_lesson
             {
                 tribblebrownSpeed.Y *= -1;
                 tribbleSound.Play();
+                backColor = Color.White;
 
             }
 
@@ -98,22 +130,53 @@ namespace animation_lesson
 
             tribbleGreyRect.Y += (int)tribblegreySpeed.Y;
 
-            if (tribbleGreyRect.Bottom > window.Height || tribbleGreyRect.Top < 0)
+            if (tribbleGreyRect.Top > window.Height)  
             {
-
-                tribblegreySpeed.Y *= -1;
+                
+                tribbleGreyRect.Y = -tribbleGreyRect.Height;
+                
+                
+                
             }
+            
+                
 
 
 
 
-            tribbleCreamRect.X += (int)tribblecreamSpeed.X;
+                tribbleCreamRect.X += (int)tribblecreamSpeed.X;
             if (tribbleCreamRect.Right > window.Width || tribbleCreamRect.Left <= 0)
             {
                 tribblecreamSpeed.X *= -1;
                 tribbleSound.Play();
+                backColor = Color.Pink;
 
             }
+
+
+
+
+            fur4Rect.X += (int)fur4Speed.X;
+            if (fur4Rect.Right > window.Width || fur4Rect.Left <= 0)
+            {
+                fur4Speed.X *= -1;
+                
+                
+
+
+            }
+
+            fur4Rect.Y += (int)fur4Speed.Y;
+
+            if (fur4Rect.Bottom > window.Height || fur4Rect.Top < 0)
+            {
+                fur4Speed.Y *= -1;
+                
+                
+
+            }
+
+            
 
 
 
@@ -125,27 +188,15 @@ namespace animation_lesson
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(backColor);
 
             _spriteBatch.Begin();
 
 
-            _spriteBatch.Draw(tribblebrownTexture, tribbleBrownRect, Color.White);
+            _spriteBatch.Draw(tribblebrownTexture, tribbleBrownRect, brownColorMask);
             _spriteBatch.Draw(tribblegreyTexture, tribbleGreyRect, Color.White);
             _spriteBatch.Draw(tribblecreamTexture, tribbleCreamRect, Color.White);
-
-
-            if (tribbleGreyRect.Bottom >=window.Height)
-            {
-                GraphicsDevice.Clear(Color.Red);
-               
-            }
-            
-
-             if (tribbleGreyRect.Top < 0)
-            {
-                GraphicsDevice.Clear(Color.Pink);
-            }
+            _spriteBatch.Draw(fur4Texture, fur4Rect, Color.White);
 
 
 
@@ -153,7 +204,11 @@ namespace animation_lesson
 
 
 
-                _spriteBatch.End();
+
+
+
+
+            _spriteBatch.End();
 
             // TODO: Add your drawing code here
 
